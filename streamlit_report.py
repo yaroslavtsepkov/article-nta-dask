62af1672173b1a6a432057215ba2e85ec6710546
yaroslavtsepkov <yaroslav.tsepkov@gmail.com>
2021-07-21 14:13:39 +0400

    add report

--- streamlit_report.py
+++ streamlit_report.py
@@ -13,23 +13,6 @@ def main():
     with st.beta_container():
         st.title("Интерактивный отчет, сравнение Dask и Pandas")
         st.markdown("На данном отчете вы можете посмотреть взаимось любых данных нашего экспереминта")
-    xaxis = "filesize(mb)"
-    yaxis = "process(mb/s)"
-    varcolor = "tools"
-    interpol = "basis"
-    agg = "median"
-    with st.beta_container():
-        st.altair_chart(
-        alt.Chart(df, width=512).mark_circle(size=60).encode(
-            x=xaxis, y=yaxis, color=varcolor, tooltip=list(df)
-        ).interactive(), False
-        )
-        st.altair_chart(
-        alt.Chart(df, width=512).mark_line(size=3, interpolate=interpol).encode(
-            x=xaxis,\
-            y='{}({})'.format(agg,yaxis), color=varcolor
-        ).interactive()
-        )
     with st.sidebar:
         st.header("Тут вы можете изменить настройки графиков")
         xaxis = st.selectbox("Ось x",df.columns)
@@ -37,7 +20,18 @@ def main():
         varcolor = st.selectbox("Как разделить наблюдения", df.drop(columns=[xaxis,yaxis]).columns)
         agg = st.selectbox("Агрегация", ["median", "mean"])
         interpol = st.selectbox("Интерполяция", ['basis', 'basis-open', 'basis-closed', 'bundle', 'cardinal', 'cardinal-open', 'cardinal-closed', 'catmull-rom', 'linear', 'linear-closed', 'monotone', 'natural', 'step', 'step-before', 'step-after'])
-   
+    with st.beta_container():
+        st.altair_chart(
+            alt.Chart(df, width=512).mark_circle(size=60).encode(
+                x=xaxis, y=yaxis, color=varcolor, tooltip=list(df)
+            ).interactive(), False
+        )
+        st.altair_chart(
+            alt.Chart(df, width=512).mark_line(size=3, interpolate=interpol).encode(
+                x=xaxis,\
+                y='{}({})'.format(agg,yaxis), color=varcolor
+            ).interactive()
+        )
         with st.beta_expander("Набор данных"):
             st.dataframe(df)
             
