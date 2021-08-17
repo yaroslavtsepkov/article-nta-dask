20074d763255b46c496eb1174c91f21fa83a76c2
yaroslavtsepkov <yaroslav.tsepkov@gmail.com>
2021-08-17 23:28:53 +0400

    fix

--- streamlit_report.py
+++ streamlit_report.py
@@ -34,7 +34,7 @@ def main():
         )
         st.altair_chart(
             alt.Chart(df, width=512).mark_bar().encode(
-                x="researcher",y="count({}".format(yaxis)
+                x="researcher",y="count({})".format(yaxis)
             )
         )
         with st.beta_expander("Набор данных"):
