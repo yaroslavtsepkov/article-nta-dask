e1dccc6b70ab8331ef37cd1626d89bacdad87160
yaroslavtsepkov <yaroslav.tsepkov@gmail.com>
2021-08-17 23:21:14 +0400

    fix

--- streamlit_report.py
+++ streamlit_report.py
@@ -10,25 +10,25 @@ def main():
     layout="wide",
     initial_sidebar_state="expanded")
     df = pd.read_csv("report.csv", sep=",")
-    with st.beta_container():
+    with st.container():
         st.title("Интерактивный отчет, сравнение Dask и Pandas")
         st.markdown("На данном отчете вы можете посмотреть взаимось любых данных нашего экспереминта")
     with st.sidebar:
         st.header("Тут вы можете изменить настройки графиков")
-        xaxis = st.selectbox("Ось x","process(mb/s)")
-        yaxis = st.selectbox("Ось y",df.drop(columns=[xaxis]).columns)
-        varcolor = st.selectbox("Как разделить наблюдения", df.drop(columns=[xaxis,yaxis]).columns)
+        #xaxis = st.selectbox("Ось x","process(mb/s)")
+        yaxis = st.selectbox("Ось y",df.drop(columns="process(mb/s)").columns)
+        varcolor = st.selectbox("Как разделить наблюдения", df.drop(columns=["process(mb/s)",yaxis]).columns)
         agg = st.selectbox("Агрегация", ["median", "mean"])
         interpol = st.selectbox("Интерполяция", ['basis', 'basis-open', 'basis-closed', 'bundle', 'cardinal', 'cardinal-open', 'cardinal-closed', 'catmull-rom', 'linear', 'linear-closed', 'monotone', 'natural', 'step', 'step-before', 'step-after'])
-    with st.beta_container():
+    with st.container():
         st.altair_chart(
             alt.Chart(df, width=512).mark_circle(size=60).encode(
-                x=xaxis, y=yaxis, color=varcolor, tooltip=list(df)
+                x="process(mb/s)", y=yaxis, color=varcolor, tooltip=list(df)
             ).interactive(), False
         )
         st.altair_chart(
             alt.Chart(df, width=512).mark_line(size=3, interpolate=interpol).encode(
-                x=xaxis,\
+                x="process(mb/s)",\
                 y='{}({})'.format(agg,yaxis), color=varcolor
             ).interactive()
         )
