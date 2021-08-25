7950f9960c0c04b2fb15a3cc7d94475a129b968e
yaroslavtsepkov <yaroslav.tsepkov@gmail.com>
2021-08-26 00:46:46 +0400

    refresh report

--- streamlit_report.py
+++ streamlit_report.py
@@ -1,43 +1,59 @@
 import pandas as pd
-from pandas.io.pytables import ClosedFileError
 import streamlit as st
 import altair as alt
 
-def main():
-    st.set_page_config(
+st.set_page_config(
     page_title="Experiment report",
     page_icon="🧊",
     layout="wide",
     initial_sidebar_state="expanded")
-    df = pd.read_csv("report.csv", sep=",")
-    with st.container():
-        st.title("Интерактивный отчет, сравнение Dask и Pandas")
-        st.markdown("На данном отчете вы можете посмотреть взаимось любых данных нашего экспереминта")
-    with st.sidebar:
-        st.header("Тут вы можете изменить настройки графиков")
-        #xaxis = st.selectbox("Ось x","process(mb/s)")
-        yaxis = st.selectbox("Ось y",df.drop(columns="filesize(mb)").columns)
-        varcolor = st.selectbox("Как разделить наблюдения", df.drop(columns=["filesize(mb)",yaxis]).columns)
-        agg = st.selectbox("Агрегация", ["median", "mean"])
-        interpol = st.selectbox("Интерполяция", ['basis', 'basis-open', 'basis-closed', 'bundle', 'cardinal', 'cardinal-open', 'cardinal-closed', 'catmull-rom', 'linear', 'linear-closed', 'monotone', 'natural', 'step', 'step-before', 'step-after'])
-    with st.container():
-        st.altair_chart(
-            alt.Chart(df, width=512).mark_circle(size=60).encode(
-                x="filesize(mb)", y=yaxis, color=varcolor, tooltip=list(df)
-            ).interactive()
-        )
-        st.altair_chart(
-            alt.Chart(df, width=512).mark_line(size=3, interpolate=interpol).encode(
-                x="filesize(mb)",\
-                y='{}({})'.format(agg,yaxis), color=varcolor
-            ).interactive()
-        )
-        with st.beta_expander("Набор данных"):
-            st.dataframe(df)
-            
-                
-                    
+    
+def main():
+	    
+	df = pd.read_csv("report.csv", sep=",", dtype={
+	"timestamp":float,
+	"tool":str,
+	"filesize(mb/s)":str,
+	"os":str,
+	"ram(gb)":str,
+	"drive type":str,
+	"process(mb/s)":float,
+	"researcher":str
+	})
+	with st.container():
+		st.title("Интерактивный отчет, сравнение Dask и Pandas")
+		st.markdown("На данном отчете вы можете посмотреть взаимось любых данных нашего экспереминта")
+		with st.expander("Нажать для просмотра всех данных"):
+			st.dataframe(df)
+	col1, col2 = st.columns(2)
+	with col1:
+		st.markdown("Результаты эксперементов. Исследователь Цепков Я.А.")
+		temp = df.query("researcher == 'y.tsepkov'")
+#		st.altair_chart(
+#			alt.chart(temp).mark_boxplot().encode(
+#			x = "filesize(mb):q", y = "process(mb/s)",\
+#			use_container_width=true)
 
+	with col2:
+		st.markdown("Результаты эксперементов. Исследователь Поручиков М.А.")
+		temp = df.query("researcher == 'm.poruchikov'")
+		st.altair_chart(
+			alt.Chart(temp).mark_line().encode(
+			x = "filesize(mb):Q", y = "process(mb/s)",color="tool", size="ram(gb)"),\
+			use_container_width=True)
+			
+	with st.container():
+		temp = df.groupby(["filesize(mb)","ram(gb)","os","drive type", "researcher"]).agg({"process(mb/s)":"mean"}).reset_index()
+		st.altair_chart(
+			alt.Chart(temp).mark_bar().encode(x="ram(gb)", y="mean(process(mb/s))",\
+		color="researcher"), use_container_width=False)
+		st.altair_chart(
+			alt.Chart(temp).mark_bar().encode(
+				x="os", y="mean(process(mb/s))",color="ram(gb)",column="researcher"),use_container_width=False)
+		with col3:
+			st.altair_chart(
+				alt.Chart(temp).mark_bar().encode(
+					x="drive type", y="mean(process(mb/s))",color="ram(gb)", column="researcher"), use_container_width=True)
 
 if __name__ == "__main__":
     main()
